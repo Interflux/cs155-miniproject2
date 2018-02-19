10312058aca7473178f8b7f8e51a4bcf072593af
Interflux <36431928+Interflux@users.noreply.github.com>
2018-02-19 05:11:27 -0800

    Add files via upload
    Added code to visualize movie ratings from selected genres.

--- basic_visualizations.py
+++ basic_visualizations.py
@@ -1,6 +1,6 @@
 """
 Filename:     basic_visualizations.py
-Version:      0.9
+Version:      1.0
 Date:         2018/2/19
 
 Description:  Generates basic visualizations for CS 155's second miniproject.
@@ -19,6 +19,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
+
+# Import packages
+import random
 
 
 def main():
@@ -139,7 +142,64 @@ def main():
     #                                                            #
     ##############################################################
 
-    # TODO
+    # Set n for the number of genres to visualize
+    n = 3
+    
+    # Define the lookup dictionary for the movie genres
+    genre_labels = {2  : "Unknown",
+                    3  : "Action",
+                    4  : "Adventure",
+                    5  : "Animation",
+                    6  : "Childrens",
+                    7  : "Comedy",
+                    8  : "Crime",
+                    9  : "Documentary",
+                    10 : "Drama",
+                    11 : "Fantasy",
+                    12 : "Film-Noir",
+                    13 : "Horror",
+                    14 : "Musical",
+                    15 : "Mystery",
+                    16 : "Romance",
+                    17 : "Sci-Fi",
+                    18 : "Thriller",
+                    19 : "War",
+                    20 : "Western"}
+
+    # Load the movie data
+    movie_data = np.loadtxt("data\\movies.txt", dtype="str", delimiter="\t")
+    
+    # Select n genres to visualize
+    genres = random.sample(range(2, 20 + 1), n)
+
+    # For each genre, visualize the ratings for all the movies in the genre
+    for genre in genres:
+        # Identify all the movies in the genre
+        genre_movies = []
+        for movie in movie_data:
+            if movie[genre] == "1":
+                genre_movies.append(np.uint16(movie[0]))
+
+        # Extract their ratings
+        genre_ratings = np.asarray([x for x in ratings if x[1] in genre_movies])
+    
+        # Count the frequency of each rating
+        genre_ratings_distribution = np.zeros(5, dtype="int")
+        for rating in genre_ratings[:, 2]:
+            genre_ratings_distribution[rating - 1] += 1
+    
+        # Generate a histogram of the genre-typed movies as a bar chart
+        plt.bar(np.arange(1, 5 + 1), genre_ratings_distribution)
+        plt.title("Distribution of Ratings for the Movies of Genre: \'" + genre_labels[genre] + "\'")
+        plt.xlabel("Rating")
+        plt.ylabel("Frequency")
+        plt.gca().grid(axis="y", linestyle="--")
+        plt.savefig("distribution_of_" + genre_labels[genre] + "_movie_ratings.png", dpi=200)
+
+        # Flush the plot
+        plt.cla()
+        plt.clf()
+        plt.close()
     
     
 if __name__ == "__main__":
