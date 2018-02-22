94693c2bf6a7f78539891fd59b11aa252fe8a1aa
Interflux <36431928+Interflux@users.noreply.github.com>
2018-02-21 22:20:51 -0800

    Add files via upload
    Added TensorFlow implementation of SVD and moved different SVD implementations into their own functions. Note there's a bug in the surprise version of SVD: the item-factor matrix has dimensions (1644, k) instead of (1682, k).

--- advanced_visualizations.py
+++ advanced_visualizations.py
@@ -1,9 +1,9 @@
 """
 Filename:     advanced_visualizations.py
-Version:      0.12
+Version:      0.2
 Date:         2018/2/21
 
-Description:  Performs operations to generate advanced visualizations for
+Description:  Tests operations to generate advanced visualizations for
               CS 155's second miniproject.
 
 Author(s):     Dennis Lam
@@ -13,6 +13,7 @@ Organization:  California Institute of Technology
 
 # Import packages as aliases
 import matplotlib.pyplot as plt
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 
@@ -21,12 +22,104 @@ import scipy.linalg
 import surprise
 
 
-"""
-INITIAL COMMIT: Just getting familiar with the surprise package and making
-sure we can actually use its functions to perform the algorithms we want
-on the data!
+def svd_surprise(k=20, bias=True, test_fraction=0.25):
+    """
+    Performs SVD on the ratings data using surprise.
 
-"""
+    """
+    # Load the data
+    reader = surprise.Reader(rating_scale=(1, 5), sep='\t')
+    data = surprise.Dataset.load_from_file('data/data.txt', reader)
+
+    # Split the data into a training set and test set
+    train_set, test_set = surprise.model_selection.train_test_split(data, test_size=test_fraction)
+
+    # Declare the model
+    model = surprise.SVD(n_factors=k, biased=bias)
+
+    # Train the model on the data
+    model.fit(train_set)
+    predictions = model.test(test_set)
+
+    # Print the accuracy of the predictions
+    print("SVD Test RMSE: " + str(surprise.accuracy.rmse(predictions, verbose=False)))
+
+    # Return U, V, the user bias terms, and the movie bias terms
+    return model.pu, model.qi, model.bu, model.bi
+
+
+def svd_hamed(k=20):
+    """
+    Performs SVD on the ratings data using TensorFlow. Credit goes to Hamed Firooz:
+    http://hameddaily.blogspot.com/2016/12/simple-matrix-factorization-with.html
+
+    """
+    # Read data
+    df = pd.read_csv('data/data.txt', sep='\t', names=['user', 'item', 'rate'])
+    mask = numpy.random.rand(len(df)) < 0.7
+    df_train = df[mask]
+
+    user_indicies = [x-1 for x in df_train.user.values]
+    item_indicies = [x-1 for x in df_train.item.values]
+    rates = df_train.rate.values
+
+    # Variables
+    feature_len = k
+    U = tf.Variable(initial_value=tf.truncated_normal([943,feature_len]), name='users')
+    P = tf.Variable(initial_value=tf.truncated_normal([feature_len,1682]), name='items')
+    result = tf.matmul(U, P)
+    result_flatten = tf.reshape(result, [-1])
+
+    # Rating
+    R = tf.gather(result_flatten, user_indicies * tf.shape(result)[1] + item_indicies, name='extracting_user_rate')
+
+    # Cost function
+    diff_op = tf.subtract(R, rates, name='training_diff')
+    diff_op_squared = tf.abs(diff_op, name="squared_difference")
+    base_cost = tf.reduce_sum(diff_op_squared, name="sum_squared_error")
+
+    # Regularization
+    lda = tf.constant(.001, name='lambda')
+    norm_sums = tf.add(tf.reduce_sum(tf.abs(U, name='user_abs'), name='user_norm'),
+                       tf.reduce_sum(tf.abs(P, name='item_abs'), name='item_norm'))
+    regularizer = tf.multiply(norm_sums, lda, 'regularizer')
+
+    # Cost function
+    lr = tf.constant(.001, name='learning_rate')
+    global_step = tf.Variable(0, trainable=False)
+    learning_rate = tf.train.exponential_decay(lr, global_step, 10000, 0.96, staircase=True)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_step = optimizer.minimize(base_cost, global_step=global_step)
+
+    # Execute
+    sess = tf.Session()
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    for i in range(1000):
+        sess.run(training_step)
+
+    # Example: user=278 item=603 r=5 (from u.data)
+    u, p, r = df[['user', 'item', 'rate']].values[0]
+    rhat = tf.gather(tf.gather(result, u-1), p-1)
+    print("rating for user " + str(u) + " for item " + str(p) + " is " + str(r) + " and our prediction is: " + str(sess.run(rhat)))
+
+    # Calculate accuracy
+    df_test = df[~msk]
+    user_indicies_test = [x-1 for x in df_test.user.values]
+    item_indicies_test = [x-1 for x in df_test.item.values]
+    rates_test = df_test.rate.values
+
+    # Accuracy
+    R_test = tf.gather(result_flatten, user_indicies_test * tf.shape(result)[1] + item_indicies_test, name='extracting_user_rate_test')
+    diff_op_test = tf.subtract(R_test, rates_test, name='test_diff')
+    diff_op_squared_test = tf.abs(diff_op, name="squared_difference_test")
+
+    cost_test = tf.divide(tf.reduce_sum(tf.square(diff_op_squared_test, name="squared_difference_test"), name="sum_squared_error_test"), df_test.shape[0] * 2., name="average_error")
+    print(sess.run(cost_test))
+
+    # Return latent vectors in NumPy format
+    return U.eval(session=sess), P.eval(session=sess)
 
 
 def print_ratings_dataframe(df):
@@ -38,15 +131,13 @@ def print_ratings_dataframe(df):
         print(df)
 
 
-""" First, load the data """
+""" If we're loading the data ourselves...
 
 # Load the movie data
 movies = np.loadtxt("data\\movies.txt", dtype="str", delimiter="\t")
 
 # Load the rating data
 ratings = np.loadtxt("data\\data.txt", dtype="int")
-
-""" Then, convert the data to a dataframe so it can be read by surprise """
 
 # Stores the ratings in a pandas-readable dictionary
 ratings_dict = {'userID'  : [],
@@ -61,54 +152,28 @@ for rating in ratings:
 
 # Load the dictionary into a dataframe
 df = pd.DataFrame(ratings_dict)
-    
-# Declare a reader with the appropriate rating scale
-reader = surprise.Reader(rating_scale=(1, 5))
 
-# Load the dataframe into a surprise-readable data structure
-data = surprise.Dataset.load_from_df(df[['userID', 'movieID', 'rating']], reader)
+"""
 
-""" Finally, let's run an SVD algorithm on the data """
+# Perform SVD using Hamed's TensorFlow implementation
+U, V = svd_hamed(k=20)
 
-# Split the data into a training set and test set
-train_set, test_set = surprise.model_selection.train_test_split(data, test_size=0.25)
+# Transpose U
+U = np.matrix.transpose(U)
 
-# Set the number of factors
-k = 20
+""" 
+# Perform SVD using surprise
+U, V, a, b = svd_surprise(k=20, bias=true)
 
-# Declare the model
-model = surprise.SVD(n_factors=k, biased=False)
+# Transpose U and V
+U = np.matrix.transpose(U)
+V = np.matrix.transpose(V)
 
-# Train the model on the data
-model.fit(train_set)
-predictions = model.test(test_set)
+"""
 
-# Print the accuracy of the predictions
-print("Unbiased-SVD Test RMSE: " + str(surprise.accuracy.rmse(predictions, verbose=False)))
+### Project U and V onto 2 dimensions ###
 
-""" Now let's run an SVD algorithm with bias terms on the data """
-
-# Declare the model
-model = surprise.SVD(n_factors=k, biased=True)
-
-# Train the model on the data
-model.fit(train_set)
-predictions = model.test(test_set)
-
-# Print the accuracy of the predictions
-print("Biased-SVD Test RMSE: " + str(surprise.accuracy.rmse(predictions, verbose=False)))
-
-""" Now for the hard part! Let's try to project U and V onto 2 dimensions """
-
-# Save U and V for readability (memory usage be damned)
-U = np.matrix.transpose(model.pu)
-V = np.matrix.transpose(model.qi)
-
-# Save the user biases and the movie biases, respectively
-a = model.bu
-b = model.bi
-
-# Run SVD on V, decomposing it into AS(B^T) wheA[re S is a diagonal matrix
+# Run SVD on V, decomposing it into AS(B^T) where S is a diagonal matrix
 A, S, B = scipy.linalg.svd(V)
 
 # Save the first two columns of A
@@ -119,3 +184,8 @@ A_proj_trans = np.matrix.transpose(A_proj)
 V_proj = np.matmul(A_proj_trans, V)
 U_proj = np.matmul(A_proj_trans, U)
 
+### Visualize our results ###
+
+# Make a crude plot of V_proj
+plt.plot(V_proj[0], V_proj[1], 'ro')
+plt.show()
