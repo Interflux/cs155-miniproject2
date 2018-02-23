ffd7179b79a64438910e1dd1f5dd41d450ae2a34
Garret <garretsullivan80@gmail.com>
2018-02-22 20:37:18 -0800

    Add SGD method with optional bias term
    Also fixed a few small things with the other algorithms

--- advanced_visualizations.py
+++ advanced_visualizations.py
@@ -25,6 +25,98 @@ import scipy.linalg
 import surprise
 
 
+def svd_sgd(k=20, reg=0.0, eta=0.03, eps=0.0001, max_epochs=300, bias=False):
+    """
+    Performs SVD on the ratings data SGD. Adapted from Homework 5 solutions.
+
+    """
+    Y_train = np.loadtxt('data/train.txt').astype(int)
+    Y_test = np.loadtxt('data/test.txt').astype(int)
+	
+    m = max(max(Y_train[:,0]), max(Y_test[:,0])).astype(int) # users
+    n = max(max(Y_train[:,1]), max(Y_test[:,1])).astype(int) # movies
+    
+    U = np.random.random((m,k)) - 0.5
+    V = np.random.random((k,n)) - 0.5
+    A = np.random.random(m) - 0.5
+    B = np.random.random(n) - 0.5
+    
+    size = Y_train.shape[0]
+    delta = None
+    indices = np.arange(size)    
+    for epoch in range(max_epochs):
+        # Run an epoch of SGD
+        before_E_in = get_err_sgd(U, V, A, B, Y_train, reg, bias)
+        np.random.shuffle(indices)
+        for ind in indices:
+            (i, j, Yij) = Y_train[ind]
+            prediction = np.dot(U[i-1], V[:,j-1])
+            
+            # If the bias terms are enabled, add them to the prediction
+            if bias:
+                prediction = prediction + A[i-1] + B[j-1]
+            
+            # Update U[i], V[j]
+            U[i-1] = U[i-1] - eta*(reg*U[i-1] - V[:,j-1]*(Yij - prediction))
+            V[:,j-1] = V[:,j-1] - eta*(reg*V[:,j-1] - U[i-1]*(Yij - prediction))
+            
+            # If the bias terms are enabled, update them as well
+            if bias:
+                A[i-1] = A[i-1] - eta*(-(Yij - prediction))
+                B[j-1] = B[j-1] - eta*(-(Yij - prediction))
+            
+        E_in = get_err_sgd(U, V, A, B, Y_train, reg, bias)
+
+        # Compute change in E_in for first epoch
+        if epoch == 0:
+            delta = before_E_in - E_in
+
+        # If E_in doesn't decrease by some fraction <eps>
+        # of the initial decrease in E_in, stop early            
+        elif before_E_in - E_in < eps * delta:
+            break
+        
+    print("SVD Test RMSE: " + str(np.sqrt(get_err_sgd(U, V, A, B, Y_test, bias=bias))))
+        
+    return U, V
+
+
+def get_err_sgd(U, V, A, B, Y, reg=0.0, bias=False):
+    """
+    Takes as input a matrix Y of triples (i, j, Y_ij) where i is the index of a user,
+    j is the index of a movie, and Y_ij is user i's rating of movie j and
+    user/movie matrices U and V.
+
+    Returns the mean regularized squared-error of predictions made by
+    estimating Y_{ij} as the dot product of the ith row of U and the jth column of V^T,
+    possibly with bias terms.
+    
+    Adapted from Homework 5 solutions.
+    
+    """
+    # Compute mean squared error on each data point in Y; include
+    # regularization penalty in error calculations.
+    # We first compute the total squared squared error
+    err = 0.0
+    for (i,j,Yij) in Y:
+        prediction = np.dot(U[i-1], V[:,j-1])
+        
+        # If the bias terms are enabled, add them to the prediction
+        if bias:
+            prediction = prediction + A[i-1] + B[j-1]
+        
+        err += 0.5 *(Yij - prediction)**2
+    # Add error penalty due to regularization if regularization
+    # parameter is nonzero
+    if reg != 0:
+        U_frobenius_norm = np.linalg.norm(U, ord='fro')
+        V_frobenius_norm = np.linalg.norm(V, ord='fro')
+        err += 0.5 * reg * (U_frobenius_norm ** 2)
+        err += 0.5 * reg * (V_frobenius_norm ** 2)
+    # Return the mean of the regularized error
+    return err / float(len(Y))
+
+
 def svd_surprise(k=20, bias=True, test_fraction=0.0):
     """
     Performs SVD on the ratings data using surprise.
@@ -63,7 +155,7 @@ def svd_hamed(k=20):
     """
     # Read data
     df = pd.read_csv('data/data.txt', sep='\t', names=['user', 'item', 'rate'])
-    mask = numpy.random.rand(len(df)) < 0.7
+    mask = np.random.rand(len(df)) < 0.7
     df_train = df[mask]
 
     user_indicies = [x-1 for x in df_train.user.values]
@@ -90,13 +182,14 @@ def svd_hamed(k=20):
     norm_sums = tf.add(tf.reduce_sum(tf.abs(U, name='user_abs'), name='user_norm'),
                        tf.reduce_sum(tf.abs(P, name='item_abs'), name='item_norm'))
     regularizer = tf.multiply(norm_sums, lda, 'regularizer')
+    cost = tf.add(base_cost, regularizer)
 
     # Cost function
     lr = tf.constant(.001, name='learning_rate')
     global_step = tf.Variable(0, trainable=False)
     learning_rate = tf.train.exponential_decay(lr, global_step, 10000, 0.96, staircase=True)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    training_step = optimizer.minimize(base_cost, global_step=global_step)
+    training_step = optimizer.minimize(cost, global_step=global_step)
 
     # Execute
     sess = tf.Session()
@@ -112,7 +205,7 @@ def svd_hamed(k=20):
     print("rating for user " + str(u) + " for item " + str(p) + " is " + str(r) + " and our prediction is: " + str(sess.run(rhat)))
 
     # Calculate accuracy
-    df_test = df[~msk]
+    df_test = df[~mask]
     user_indicies_test = [x-1 for x in df_test.user.values]
     item_indicies_test = [x-1 for x in df_test.item.values]
     rates_test = df_test.rate.values
@@ -120,7 +213,7 @@ def svd_hamed(k=20):
     # Accuracy
     R_test = tf.gather(result_flatten, user_indicies_test * tf.shape(result)[1] + item_indicies_test, name='extracting_user_rate_test')
     diff_op_test = tf.subtract(R_test, rates_test, name='test_diff')
-    diff_op_squared_test = tf.abs(diff_op, name="squared_difference_test")
+    diff_op_squared_test = tf.abs(diff_op_test, name="squared_difference_test")
 
     cost_test = tf.divide(tf.reduce_sum(tf.square(diff_op_squared_test, name="squared_difference_test"), name="sum_squared_error_test"), df_test.shape[0] * 2., name="average_error")
     print(sess.run(cost_test))
@@ -172,12 +265,23 @@ U = np.matrix.transpose(U)
 
 """
 
+""" If we want to perform SVD using surprise...
+
 # Perform SVD using surprise
 U, V, a, b = svd_surprise(k=20, bias=True)
 
 # Transpose U and V
 U = np.matrix.transpose(U)
 V = np.matrix.transpose(V)
+
+"""
+
+# Perform SVD using SGD
+U, V = svd_sgd(k=20, reg=10**-1, bias=True)
+
+# Transpose U
+U = np.matrix.transpose(U)
+
 
 ### Project U and V onto 2 dimensions ###
 
